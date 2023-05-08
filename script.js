b65f7ccfa90e70b2fe4e030c338b636fbd617f79
OniChan-Baka <famalik9876@gmail.com>
2023-05-08 06:39:17 -0700

    finshed here to day

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+import * as words from "\rec\words.txt";
+
+
 const text = document.getElementById("textTag")
 const cat = document.getElementById("cat")
 
 
-const scr1 = "Web\TypingSpeed\res\1.png"
-const scr2 = "Web\TypingSpeed\res\2.png"
+const scr1 = "\rec\1.png"
+const scr2 = "\rec\2.png"
+
+
